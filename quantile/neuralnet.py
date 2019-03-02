522be8ecd20da319fb0a5ae2c9b51dbe05cd9d40
mickey <mickey.a.warner@gmail.com>
2019-03-02 12:34:52 -0700

    clean up, add second example

--- quantile/neuralnet.py
+++ quantile/neuralnet.py
@@ -4,161 +4,150 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import sobol_seq
-from scipy.stats import norm
 from keras.models import Model
-from keras.layers import Dense, Activation, Input, Dropout
+from keras.layers import Dense, Activation, Input, Dropout, BatchNormalization
 from keras import backend as K
+
+from scipy.stats import norm
 from keras import optimizers
 from tensorflow import lgamma
 
 
-### For splitting into train and test sets
-def get_train_test_inds(n, train_proportion):
-    train_n = round(n * train_proportion)
-    full_ind = np.random.permutation(n)
-    train_ind = full_ind[:train_n]
-    test_ind = full_ind[train_n:]
-    return train_ind, test_ind
-
 ### Custom loss functions using negative log-likelihood
 def make_quantile(q):
     q = np.array(q)
-    m = len(q)
     def loss(y_true, y_pred):
         e = y_true - y_pred
         return K.mean(K.maximum(q*e, (q-1)*e), axis = -1)
     return loss
 
 
-### Generate data, simple
+### Generate data
 np.random.seed(1)
-n = 62000
+n = 50000
 k = 10
-X = np.random.rand(n, k) * 3
+x = np.random.rand(n, k) * 3
 
-xsum = np.sum(X[:,0:4], axis = 1)
-xprod = np.sum(X[:,2:6], axis = 1)
+xsum = np.sum(x[:,0:4], axis = 1)
+xprod = np.sum(x[:,2:6], axis = 1)
 
-Y = np.random.normal(loc = xsum, scale = xprod / 10.)
+y = np.random.normal(loc = xsum, scale = xprod / 10.)
 
 plt.clf()
-plt.scatter(xsum, Y)
+_ = plt.scatter(xsum, y)
 plt.show(block = False)
 
 plt.clf()
-plt.scatter(xprod, Y)
+_ = plt.scatter(xprod, y)
 plt.show(block = False)
-
-# Reshape Y (necessary?)
-#Y = Y.reshape(Y.shape[0], 1)
 
 ### Separate into training and test sets
-np.random.seed(123)
-train_ind, test_ind = get_train_test_inds(n, 0.9)
+m = int(np.round(n * 0.9))
+x_train = x[:m,:]
+y_train = y[:m]
 
-X_train = X[train_ind,]
-X_test = X[test_ind,]
-
-Y_train = Y[train_ind]
-Y_test = Y[test_ind]
+x_test = x[m:,:]
+y_test = y[m:]
 
 q = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 qloss = make_quantile(q)
 
 ### Build the neural network architecture
-inputs = Input(shape=(X_train.shape[1],), name = 'input')
-layer = Dense(units = 64, activation = 'relu', name = 'hidden_layer_1')(inputs)
+inputs = Input(shape=(k,))
+layer = Dense(units = 64, activation = 'relu')(inputs)
 layer = Dropout(0.2)(layer)
-layer = Dense(units = 64, activation = 'relu', name = 'hidden_layer_2')(layer)
+layer = Dense(units = 64, activation = 'relu')(layer)
 layer = Dropout(0.2)(layer)
-output = Dense(units = len(q), activation = 'linear', name = 'out')(layer)
-model = Model(inputs = inputs, outputs = output, name = 'mean_var')
+output = Dense(units = len(q), activation = 'linear')(layer)
+model = Model(inputs = inputs, outputs = output)
 
-model.compile(
-    optimizer = 'adagrad',
-    loss = qloss,
-    metrics = ['accuracy'])
+model.compile(optimizer = 'adagrad', loss = qloss, metrics = ['accuracy'])
 
-model.fit(x = {'input' : X_train},
-    y = {'out' : Y_train},
-    epochs = 20, batch_size = 128, validation_split = 0.1)
+model.fit(x = x_train, y = y_train, epochs = 20, batch_size = 128, validation_split = 0.1)
 
 ### Make predictions
-Y_pred = model.predict(x = X_test)
-error = np.zeros((len(test_ind), len(q)))
-truth = np.zeros((len(test_ind), len(q)))
+Y_pred = model.predict(x = x_test)
+error = np.zeros((n-m, len(q)))
+truth = np.zeros((n-m, len(q)))
 for i in range(len(q)):
-    truth[:,i] = xsum[test_ind] + xprod[test_ind] / 10. * norm.isf(1-q[i])
+    truth[:,i] = xsum[m:] + xprod[m:] / 10. * norm.isf(1-q[i])
 
-Y_pred
-truth
+plt.clf()
+plt.scatter(xsum[m:], y[m:])
+plt.scatter(xsum[m:], Y_pred[:,4])
+plt.scatter(xsum[m:], Y_pred[:,0])
+plt.scatter(xsum[m:], Y_pred[:,8])
+plt.show(block = False)
 
-i = 8
-np.sqrt(np.mean((Y_pred[:,i] - truth[:,i])**2))
-
-np.sqrt(np.mean((Y_pred[:,0] - (xsum[test_ind] + error[:,0]))**2))
-np.sqrt(np.mean((Y_pred[:,1] - (xsum[test_ind] + error[:,1]))**2))
-np.sqrt(np.mean((Y_pred[:,2] - (xsum[test_ind] + error[:,2]))**2))
-
-np.sqrt(np.mean((Y_pred[:,0] - (Y_test + error[:,0]))**2))
-np.sqrt(np.mean((Y_pred[:,1] - (Y_test + error[:,1]))**2))
-np.sqrt(np.mean((Y_pred[:,2] - (Y_test + error[:,2]))**2))
-
-Y_pred[:,1]
-Y_test
-
-
-
-
-# Compare predicted mean with predicted var
-plt.clf();
-plt.plot(Y_pred_in[:,0], Y_pred_in[:,1], marker='s', linestyle='',
+plt.plot(Y_pred[:,0], Y_pred_in[:,1], marker='s', linestyle='',
 color = (0,0.5,0,0.3))
 plt.plot(Y_pred[:,0], Y_pred[:,1], marker='s', linestyle='', color =
 (0,0,0.8,0.5))
 plt.show(block = False);
 
-# Compare predicted mean with true mean
-plt.clf();
-a,b = Y_pred_in[:,0].min(), Y_pred_in[:,0].max()
-plt.plot([a,b],[a,b], linestyle = '-', color = (1,0,0,0.5))
-plt.plot(Y_pred_in[:,0], xprod[train_ind,], marker='s', linestyle='',
-color = (0,0.5,0,0.3))
-plt.plot(Y_pred[:,0], xprod[test_ind,], marker='s', linestyle='',
-color = (0,0,0.8,0.3))
-plt.show(block = False);
 
-# Compare predicted sd with true sd
-plt.clf();
-a,b = 0,1
-plt.plot([a,b],[a,b], linestyle = '-', color = (1,0,0,0.5))
-plt.plot(np.sqrt(Y_pred_in[:,1]), xsum[train_ind,] / 10., marker='s',
-linestyle='', color = (0,0.5,0,0.3))
-plt.plot(np.sqrt(Y_pred[:,1]), xsum[test_ind,] / 10., marker='s',
-linestyle='', color = (0,0,0.8,0.3))
-plt.show(block = False);
 
-# See if the numbers make sense
-np.mean(Y_pred_in, 0)
-np.mean(Y_pred, 0)
-np.var(Y_pred_in, 0)
-np.var(Y_pred, 0)
 
-np.mean(Y_train, 0)
-np.mean(Y_test, 0)
-np.var(Y_train, 0)
-np.var(Y_test, 0)
+### Generate data
+np.random.seed(1)
+n = 50000
+k = 1
+x = np.random.rand(n, k) * 4 * np.pi
 
-### Plot test samples with -/+ 2 SD bounds
-plt.clf();
-a,b = Y.min(), Y.max()
-plt.plot([a,b],[a,b], linestyle = '-', color = (1,0,0,0.5))
-plt.plot(Y_test, Y_pred[:,0] + 2*Y_pred[:,1], marker='s',
-linestyle='', color=(1,0,0,0.3))
-plt.plot(Y_test, Y_pred[:,0] - 2*Y_pred[:,1], marker='s',
-linestyle='', color=(1,0,0,0.3))
-#plt.plot(Y_train[:,0], Y_pred_in[:,0], marker='s', linestyle='',
-color = (0,0.5,0,0.3))
-plt.plot(Y_test, Y_pred[:,0], marker='s', linestyle='', color = (0,0,0.8,0.5))
-plt.show(block = False);
+y = np.random.normal(loc = np.cos(x), scale = x / 10.)
+
+plt.clf()
+_ = plt.scatter(x, y)
+plt.show(block = False)
+
+### Separate into training and test sets
+m = int(np.round(n * 0.9))
+x_train = x[:m,:]
+y_train = y[:m]
+
+x_test = x[m:,:]
+y_test = y[m:]
+
+q = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+qloss = make_quantile(q)
+
+### Build the neural network architecture
+inputs = Input(shape=(k,))
+layer = Dense(units = 128, activation = 'relu')(inputs)
+layer = BatchNormalization()(layer)
+layer = Dropout(0.2)(layer)
+layer = Dense(units = 128, activation = 'relu')(layer)
+layer = BatchNormalization()(layer)
+layer = Dropout(0.2)(layer)
+layer = Dense(units = 128, activation = 'relu')(layer)
+layer = BatchNormalization()(layer)
+layer = Dropout(0.2)(layer)
+output = Dense(units = len(q), activation = 'linear')(layer)
+model = Model(inputs = inputs, outputs = output)
+model.compile(optimizer = 'adadelta', loss = qloss, metrics = ['accuracy'])
+model.fit(x = x_train, y = y_train, epochs = 100, batch_size = 128, validation_split = 0.1)
+
+### Make predictions
+Y_pred = model.predict(x = x_test)
+
+plt.clf()
+plt.scatter(x[m:], y[m:])
+plt.scatter(x[m:], Y_pred[:,4])
+plt.scatter(x[m:], Y_pred[:,0])
+plt.scatter(x[m:], Y_pred[:,8])
+plt.show(block = False)
+
+
+#error = np.zeros((n-m, len(q)))
+truth = np.zeros((n-m, len(q)))
+for i in range(len(q)):
+    truth[:,i,None] = np.cos(x[m:,:]) + x[m:,:] / 10. * norm.isf(1-q[i])
+
+plt.clf()
+plt.scatter(x[m:], Y_pred[:,4], color = (1,0,0))
+plt.scatter(x[m:], Y_pred[:,0], color = (1,0,0))
+plt.scatter(x[m:], Y_pred[:,8], color = (1,0,0))
+plt.scatter(x[m:], truth[:,4], color = (0,0,1))
+plt.scatter(x[m:], truth[:,0], color = (0,0,1))
+plt.scatter(x[m:], truth[:,8], color = (0,0,1))
+plt.show(block = False)
