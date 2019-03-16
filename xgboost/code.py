bc47c4f8add69f35dab9abb433db5a08e63aca8e
mickey <mickey.a.warner@gmail.com>
2019-03-16 09:39:32 -0600

    model training and plots

--- xgboost/code.py
+++ xgboost/code.py
@@ -1,22 +1,68 @@
 import xgboost as xgb
 import numpy as np
+import matplotlib.pyplot as plt
 
 ### Generate data
 np.random.seed(0)
-n = 5000
-k = 5
+n = 10000
+k = 10
 
-# Train data
-x_train = np.random.randn(n, k)
-y_train = np.sin(x_train[:,0]) + np.cos(np.pi * x_train[:,1]) - x_train[:,2] ** 2 - x_train[:,3]
-y_train = y_train.reshape((n, 1))
+x = np.random.randn(n, k)
+y = np.sin(x[:,0]) + np.cos(np.pi * x[:,1] + np.abs(x[:,2])) - x[:,3] ** 2 - x[:,4]
+y = y.reshape((n, 1))
 
-# Test set
+# Train and test sets
 m = 1000
-x_test = np.random.randn(m, k)
-y_test = np.sin(x_test[:,0]) + np.cos(np.pi * x_test[:,1]) - x_test[:,2] ** 2 - x_test[:,3]
-y_test = y_test.reshape((m, 1))
+x_train = x[:(n-m),:]
+y_train = y[:(n-m),:]
+x_test = x[(n-m):,:]
+y_test = y[(n-m):,:]
 
 
+### Model training
 dtrain = xgb.DMatrix(x_train, label = y_train)
+dtest = xgb.DMatrix(x_test, label = y_test)
+param = {'max_depth': 5, 'eta': 1, 'verbosity': 2, 'objective': 'reg:linear'}
+
+num_round = 10
+bst = xgb.train(param, dtrain, num_round)
+
+
+### Predictions
+y_pred = bst.predict(dtest).reshape((m,1))
+
+
+### Plots
+# Observed vs fitted
+a = min(np.min(y_test), np.min(y_pred))
+b = max(np.max(y_test), np.max(y_pred))
+plt.clf()
+plt.scatter(y_test, y_pred)
+plt.plot([a,b],[a,b], color = (1,0.5,0))
+plt.show(block = False)
+
+
+# Importance
+# I do my own plot here since xgb.plot_importance creates a new figure each time
+# we plot, and it also sorts by the importance value (I want to sort by variable
+# instead). Normally, xgb.plot_importance is fine, but in this example it's just
+# easier to see that how the first five variables differ from the last five.
+def myplot(importance_type = 'weight'):
+    imp = bst.get_score(importance_type = importance_type)
+    sort_key = sorted(imp.keys())
+    sort_val = []
+    for key in sorted(imp):
+        sort_val.append(imp[key])
+    sort_key.reverse()
+    sort_val.reverse()
+    plt.clf()
+    plt.barh(sort_key, sort_val)
+    plt.show(block = False)
+
+
+myplot('weight')
+myplot('gain')
+myplot('cover')
+myplot('total_gain')
+myplot('total_cover')
 
